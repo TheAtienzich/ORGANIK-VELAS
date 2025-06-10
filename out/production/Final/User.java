c588597567b7352c536c050210a8454e4ece0550
atien <atienzaalvaro10@gmail.com>
2025-06-10 13:08:25 +0200

    ADDING NEW FILES

---
+++ out/production/Final/User.java
@@ -0,0 +1,51 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class User {
+    protected String email;
+    protected String passWord;
+    protected String name;
+
+    public User(String email, String passWord, String name) {
+        this.email = email;
+        this.passWord = passWord;
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassWord() {
+        return this.passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public static boolean emailValido(String email) {
+        Pattern p = Pattern.compile("^[a-zA-Z0-9._%+-]+@(gmail\\.com|hotmail\\.com|onFire\\.com)$");
+        Matcher m = p.matcher(email);
+        return m.find();
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String AFichero() {
+        return this.email + ";" + this.passWord + ";" + this.name;
+    }
+
+    public String ToString() {
+        return "email: " + this.email + " Nombre: " + this.name;
+    }
+
+    public static void guardarUsuario(String fichero, String contenido) {
+    }
+}
